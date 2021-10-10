4d6baf44bcf0cb596ea17de8be3e484ddf26cc9f
Voinov Nikita <voinov.nikita2015@yandex.ru>
2021-10-11 00:34:12 +0300

    second commit

--- src/js/__tests__/map.test.js
+++ src/js/__tests__/map.test.js
@@ -1,13 +1,14 @@
-import ErrorReposoitory from "../map";
+/* eslint-disable linebreak-style */
+import ErrorReposoitory from '../map';
 
-test ('error text', () => {
-    const error = new ErrorReposoitory();
-    error. mapError(502, 'server error');
-    expect(error.translate(502)).toBe('server error')
+test('error text', () => {
+  const error = new ErrorReposoitory();
+  error.mapError(502, 'server error');
+  expect(error.translate(502)).toBe('server error');
 });
 
 
 test('error unknown', () => {
-    const error = new ErrorReposoitory();
-    expect(error.translate('')).toBe('Unknown error')
-})
+  const error = new ErrorReposoitory();
+  expect(error.translate('')).toBe('Unknown error');
+});
